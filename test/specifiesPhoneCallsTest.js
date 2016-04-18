94ce15368fd39367df60d33dc846e7a29821e17d
Nurha <nurha@projectcodex.co>
2016-04-18 15:00:52 +0200

    created tests for specified network providers

--- test/specifiesPhoneCallsTest.js
+++ test/specifiesPhoneCallsTest.js
@@ -5,14 +5,19 @@ var specifiesPhoneCall = require('../routes/specifiesPhoneCall');
 describe('find calls made to a specific network provider', function(){
 
   it('return all calls made to mtn', function(done){
-      assert.equal();
+    var callsMade = readCSVFolder.readCSVFolder('./ItemisedBill.csv');
+      assert.equal(specifiesPhoneCall.specifiesPhoneCall(callsMade, 'MTN').length, 16);
       done();
   });
+
   it('return all calls made to vodacom', function(done){
-    assert.equal();
+    var callsMade = readCSVFolder.readCSVFolder('./ItemisedBill');
+    assert.equal(specifiesPhoneCall.specifiesPhoneCall(callsMade, 'Vodacom').length, 8);
     done();
   });
-  it('return all calls  made to cellC' function(done){
-    assert.equal();
+
+  it('return all calls  made to cellC', function(done){
+    var callsMade = readCSVFolder.readCSVFolder('./ItemisedBill');
+    assert.equal(specifiesPhoneCall.specifiesPhoneCall(callsMade, 'CellC').length, 11);
   });
 });
