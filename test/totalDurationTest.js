097211c67fc660b2ad7b9807b17f1c37c0637562
Nurha <nurha@projectcodex.co>
2016-04-21 15:13:22 +0200

    total call duration test done

---
+++ test/totalDurationTest.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var readCSVFolder = require('../routes/readCSVFolder');
+var specifiesPhoneCall = require('../routes/specifiesPhoneCall');
+var callDuration = require('../routes/callDuration');
+var callsAscending = require('../routes/callsAscending');
+var totalDuration = require('../routes/totalDuration');
+
+describe('get the total duration of talking time', function() {
+
+  it('total the duration of calls for each provider listes', function(done) {
+    var callsMade = readCSVFolder.readCSVFolder('./ItemisedBill.csv');
+    var specifiesPhone = specifiesPhoneCall.specifiesPhoneCall(callsMade, 'MTN');
+    var callDurationList = callDuration.calculateCallDuration(specifiesPhone);
+    var callsAscendingOrder = callsAscending.sortInAscendingOrder(callDurationList);
+    var results = [{ provider: 'MTN', duration: 3471 },
+                   { provider: 'Vodacom', duration: 610 },
+                   { provider: 'CellC', duration: 2847 }];
+    assert.deepEqual(totalDuration.calculateTotalDuration(callsAscendingOrder), results);
+    done();
+  });
+});
