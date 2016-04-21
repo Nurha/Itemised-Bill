695e6352893f188abf23b1c794eafbf1f29d2aff
Nurha <nurha@projectcodex.co>
2016-04-21 14:00:17 +0200

    cleaned up code

--- routes/callsAscending.js
+++ routes/callsAscending.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var sortInAscendingOrder = function(callDuration) {
   console.log(callDuration);
   var sorted = callDuration.sort(function(a, b) {
-    return(a.duration - b.duration)
+    return (a.duration - b.duration)
   });
   return sorted;
 };
