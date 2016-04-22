03d4eaaf6c9b6cbbe13d0083ecddcc2195f2bd86
Nurha <nurha@projectcodex.co>
2016-04-22 11:52:46 +0200

    changed ascending order test

--- routes/callsAscending.js
+++ routes/callsAscending.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
-var sortInAscendingOrder = function(callDuration) {
-  // console.log(callDuration);
-  var sorted = callDuration.sort(function(a, b) {
+var sortInAscendingOrder = function(callsMade) {
+
+  var sorted = callsMade.sort(function(a, b) {
     return (a.duration - b.duration)
   });
+  console.log(sorted);
   return sorted;
 };
 module.exports.sortInAscendingOrder = sortInAscendingOrder;
--- routes/totalDuration.js
+++ routes/totalDuration.js
@@ -1,15 +1,15 @@
-var fs = require('fs');
-
-var calculateTotalDuration = function(callsAscending){
-  var totalCallDuration = [];
-  var totalTime = 0;
-  for(x=0; x < callsAscending.length; x++){
-    var time = parseInt(callsAscending[x].duration);
-    totalTime += time;
-  }
-  console.log(callsAscending[0].provider);
-  var duration = { provider: callsAscending[0].provider, duration : totalTime};
-  totalCallDuration.push(duration);
-  return totalCallDuration;
-};
-module.exports.calculateTotalDuration = calculateTotalDuration;
+// var fs = require('fs');
+//
+// var calculateTotalDuration = function(callsAscending){
+//   var totalCallDuration = [];
+//   var totalTime = 0;
+//   for(x=0; x < callsAscending.length; x++){
+//     var time = parseInt(callsAscending[x].duration);
+//     totalTime += time;
+//   }
+//
+//   var duration = { provider: callsAscending[0].provider, duration : totalTime};
+//   totalCallDuration.push(duration);
+//   return totalCallDuration;
+// };
+// module.exports.calculateTotalDuration = calculateTotalDuration;
